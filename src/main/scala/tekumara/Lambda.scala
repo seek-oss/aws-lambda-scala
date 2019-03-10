d7858a90087ceed8a61dcc28293eeb36025be169
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2019-03-10 19:41:35 +1100

    Revert "add jvmId to identify jvm instance in the logs"
    This reverts commit 303b5c66
    
    Decided log stream name is probably sufficient in most cases, so a better default is to not record jvmId

--- src/main/scala/tekumara/Lambda.scala
+++ src/main/scala/tekumara/Lambda.scala
@@ -2,14 +2,10 @@ package tekumara
 
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 import com.typesafe.scalalogging.StrictLogging
-import org.slf4j.MDC
 
 class Lambda extends RequestHandler[Any, Any] with StrictLogging {
 
-  val jvmId = System.currentTimeMillis()
-  MDC.put("jvmId", jvmId.toString)
-
-  logger.info(s"Lambda initialised")
+  logger.info("Lambda initialised")
 
   override def handleRequest(input: Any, context: Context): Any = {
     logger.info(s"Received ${input.getClass}: ${input.toString}")
