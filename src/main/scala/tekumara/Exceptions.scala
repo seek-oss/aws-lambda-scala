b8e1387d3bd04d5e9b11c53129066f1d1f1c7c9c
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2019-03-30 20:22:21 +1100

    improve comments, remove unnecessary brackets

--- src/main/scala/tekumara/Exceptions.scala
+++ src/main/scala/tekumara/Exceptions.scala
@@ -2,16 +2,17 @@ package tekumara
 
 object Exceptions {
 
-  // remove aws lambda runtime stack lines from the stack trace
+  // remove aws lambda runtime stack lines from the stack trace, ie: everything from the first lambdainternal line
+  // this is applied recursively to the Throwable and all its causes
+  //
+  // this mimics what the lambda runtime does when printing the stack trace for exceptions
   def filterStackTrace[T <: Throwable](t: T): T = {
 
     def removeLambdaInternal(t: T): Unit = {
       val trace = t.getStackTrace
 
       var i = 0
-      while ( {
-        i < trace.length
-      }) {
+      while (i < trace.length) {
         if (trace(i).getClassName.startsWith("lambdainternal")) {
           val newTrace = new Array[StackTraceElement](i)
           System.arraycopy(trace, 0, newTrace, 0, i)
