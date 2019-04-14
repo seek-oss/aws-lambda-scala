68488b25641a0a3d5763b824f308abdafd08bfb6
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2019-04-14 21:35:54 +1000

    more structured Splunk friendly logging

--- src/main/scala/tekumara/Lambda.scala
+++ src/main/scala/tekumara/Lambda.scala
@@ -6,20 +6,25 @@ import com.amazonaws.services.lambda.runtime.Context
 import com.typesafe.scalalogging.StrictLogging
 import ujson.Value
 import Inputs._
+import org.slf4j.MDC
 
 class Lambda extends RequestUjsonHandler with StrictLogging {
 
-  logger.info(s"Lambda initialised version ${System.getenv("version")}")
+  MDC.put("version", System.getenv("version"))
+  logger.info(s"Lambda initialised")
 
   override def handleRequest(json: Value, writer: OutputStreamWriter, context: Context): Unit = {
     json match {
       case ujson.Str("exit") =>
+        logger.info("exit")
         System.exit(1)
       case ujson.Str(s) =>
+        logger.info(s)
         writer.write(s)
       case SqsRecords(records) =>
+        val record = records(0).obj
         logger.info(s"input=${json.render()}")
-        val body = records(0).obj("body")
+        val body = record("body")
         logger.info(s"body=${body.render()}")
         ujson.writeTo(body, writer)
       case _ => throw new RuntimeException(s"Unexpected input ${json.render()}")
